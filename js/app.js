25a44ce201054175e1fe5817d7c13093845bf1a5
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-22 19:08:37 +0100

    REWORK notifications color change

--- js/app.js
+++ js/app.js
@@ -13,17 +13,17 @@ const { createApp } = Vue
             messages: [
               {
                 date: '10/01/2020 15:30:55',
-                message: 'Hai portato a spasso il cane?',
+                message: 'Hai visto il mio video sulla Gigafactory?',
                 status: 'sent'
               },
               {
                 date: '10/01/2020 15:50:00',
-                message: 'Ricordati di stendere i panni',
+                message: 'Ricordati di stare attento a ChatGpt',
                 status: 'sent'
               },
               {
                 date: '10/01/2020 16:15:22',
-                message: 'Tutto fatto!',
+                message: 'Stai tranquillo bro!',
                 status: 'received'
               }
             ],
@@ -35,17 +35,17 @@ const { createApp } = Vue
             messages: [
               {
                 date: '20/03/2020 16:30:00',
-                message: 'Ciao come stai?',
+                message: 'Yo!, hai visto Jotaro?',
                 status: 'sent'
               },
               {
                 date: '20/03/2020 16:30:55',
-                message: 'Bene grazie! Stasera ci vediamo?',
+                message: 'Hey Dio, stava venendo da te mi pare...',
                 status: 'received'
               },
               {
                 date: '20/03/2020 16:35:00',
-                message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                message: 'Nani? Masaka',
                 status: 'sent'
               }
             ],
@@ -57,17 +57,17 @@ const { createApp } = Vue
             messages: [
               {
                 date: '28/03/2020 10:10:40',
-                message: 'La Marianna va in campagna',
+                message: 'Questo esercizio non riesco a farlo!',
                 status: 'received'
               },
               {
                 date: '28/03/2020 10:20:10',
-                message: 'Sicuro di non aver sbagliato chat?',
+                message: 'Io lo finisco in 2 minuti...',
                 status: 'sent'
               },
               {
                 date: '28/03/2020 16:15:22',
-                message: 'Ah scusa!',
+                message: 'Ti rispetto.',
                 status: 'received'
               }
             ],
